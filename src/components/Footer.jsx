62d0c5157a04ecbc89a4cb59c3fde1b3570bcfd3
Mayank Rajput (HackElite) <78653024+hackelite01@users.noreply.github.com>
2022-08-02 21:54:40 +0530

    Footer Update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
   return (
     <Container as='footer' role='contentinfo' w='full' color='gray.600' p={4} alignSelf='center' pt={6} maxW='4xl'>
       <Text textAlign='center'>
-        built in sf &amp; munich by{' '}
+        built in Gujarat, India 🇮🇳 by{' '}
         <Link isExternal href={'https://instagram.com/hackelite01'} onClick={trackSocialClick}>
           @hackelite01
         </Link>{' '}
