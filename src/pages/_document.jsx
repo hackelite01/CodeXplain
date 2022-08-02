e84086cc11388fb7cbc5c487410fe612c1c3c2d4
Mayank Rajput (HackElite) <78653024+hackelite01@users.noreply.github.com>
2022-08-02 21:41:36 +0530

    Img content

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -24,15 +24,15 @@ class AppDocument extends Document {
 
           <meta property='og:url' content={process.env.NEXT_PUBLIC_VERCEL_URL} />
           <meta property='og:type' content='website' />
-          <meta property='og:title' content='CodeMate - code explained' />
+          <meta property='og:title' content='CodeXplain - code explained by AI' />
           <meta property='og:description' content='Understand code quicker. Get coding faster.' />
-          <meta property='og:site_name' content='CodeAi' />
-          <meta property='og:image' content='https://i.ibb.co/FxKj9R6/codemateogimage.png' />
+          <meta property='og:site_name' content='CodeXplain' />
+          <meta property='og:image' content='https://telegra.ph/file/6ca1415b56578b747a42d.jpg' />
           <meta name='twitter:card' content='summary_large_image' />
           <meta name='twitter:site' content='@hackelite01' />
           <meta name='twitter:title' content='CodeXplain - code explained.' />
-          <meta name='twitter:description' content='Understand code in seconds - powered by AI.' />
-          <meta name='twitter:image' content='https://i.ibb.co/FxKj9R6/codemateogimage.png' />
+          <meta name='twitter:description' content='Understand code in seconds - Developed by Mayank Rajput.' />
+          <meta name='twitter:image' content='https://telegra.ph/file/6ca1415b56578b747a42d.jpg' />
         </Head>
 
         <body>
